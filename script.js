4de350d7b3996bb79258e51a7e34c91a7e8c1b82
waiting1619 <waiting1619@gmail.com>
2026-06-13 04:05:50 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const observer = new IntersectionObserver((entries)=>{
+  entries.forEach(entry=>{
+    if(entry.isIntersecting){
+      entry.target.classList.add('show');
+    }
+  });
+},{threshold:0.15});
+
+document.querySelectorAll('.reveal').forEach(el=>observer.observe(el));
